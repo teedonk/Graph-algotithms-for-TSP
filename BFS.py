488af0d29b3de8ca5673c557868b4091afb0b6e3
teedonk <teedonk@gmail.com>
2024-02-26 03:42:28 +0000

    bfs algorithm

--- BFS.py
+++ BFS.py
@@ -7,3 +7,14 @@ from collections import deque
 def load_dataset(file_path):     #load the dataset
     dataset = pd.read_csv(file_path, header=None, sep='\s+')
     return dataset.values.tolist()
+def bfs(graph, visualize=True): #dfs algorithm
+    num_nodes = len(graph)
+
+    # Initialize visited nodes and distances
+    visited = [False] * num_nodes
+    distances = [float('inf')] * num_nodes
+
+    # Start from the first node
+    start_node = 0
+    visited[start_node] = True
+    distances[start_node] = 0
