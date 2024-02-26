e9a759fec543ce223b7b3d41d11626c352688665
teedonk <teedonk@gmail.com>
2024-02-26 03:40:34 +0000

    function to load datset

---
+++ BFS.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import deque
+
+
+def load_dataset(file_path):     #load the dataset
+    dataset = pd.read_csv(file_path, header=None, sep='\s+')
+    return dataset.values.tolist()
