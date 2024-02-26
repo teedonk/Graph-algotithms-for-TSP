68e5c7f6fbad303d045b671aff567f4dd5ddc49d
teedonk <teedonk@gmail.com>
2024-02-26 03:56:34 +0000

    Dijksra's algorithmfor tsp

--- DIJKSTRA'S.py
+++ DIJKSTRA'S.py
@@ -36,3 +36,42 @@ def dijkstra(graph, start):
                     heapq.heappush(heap, (new_distance, neighbor))
 
     return distances
+def tsp(graph, visualize=True):
+    num_nodes = len(graph)
+
+    # Start from the first node
+    start_node = 0
+
+    # Run Dijkstra's algorithm to find the optimal path
+    distances = dijkstra(graph, start_node)
+
+    # Find the minimum distance Hamiltonian cycle using the distances
+    path = [start_node]
+    current_node = start_node
+
+    for _ in range(num_nodes - 1):
+        next_node = min((n for n in range(num_nodes) if n not in path), key=lambda x: distances[current_node] + graph[current_node][x])
+        path.append(next_node)
+        current_node = next_node
+
+    # Visualization setup
+    G = nx.Graph()
+    G.add_nodes_from(range(num_nodes))
+    edge_labels = {}
+
+    if visualize:
+        pos = nx.spring_layout(G)
+
+    for i in range(len(path) - 1):
+        G.add_edge(path[i], path[i + 1])
+        edge_labels[(path[i], path[i + 1])] = graph[path[i]][path[i + 1]]
+
+    if visualize:
+        nx.draw(G, pos, with_labels=True, font_weight='bold')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        plt.show()
+
+    total_distance = calculate_total_distance(path, graph)
+    print("Optimal Path:", path)
+    print("Total Distance:", total_distance)
+    return total_distance
